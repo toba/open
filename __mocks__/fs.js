f681e75a50772d6561cdba73efe36650a217d12d
Jason Abbott <dev@trailimage.com>
2018-04-14 15:55:00 -0600

    another try at fixing tests

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,11 +1,14 @@
 const fs = jest.genMockFromModule('fs');
-const wslProcVersion =
-   'Linux version 3.4.0-Microsoft (Microsoft@Microsoft.com) (gcc version 4.7 (GCC) ) #1 SMP PREEMPT Wed Dec 31 14:42:53 PST 2014';
+const content = {};
+
+fs.__setFileContent = function(path, text) {
+   content[path] = text;
+};
 
 /**
  * Replace output to match `is-wsl#checkIsWSL()` condition for Windows Subsystem
  * for Linux.
  */
-fs.readFileSync = () => wslProcVersion;
+fs.readFileSync = path => content[path];
 
 module.exports = fs;
--- lib/is-wsl.test.ts
+++ lib/is-wsl.test.ts
@@ -1,8 +1,13 @@
 import '@toba/test';
+import * as fs from 'fs';
 import { checkIsWSL } from './is-wsl';
 import { Platform } from './types';
 
 jest.mock('fs');
+
+const fileProc = '/proc/version';
+const wslProcVersion =
+   'Linux version 3.4.0-Microsoft (Microsoft@Microsoft.com) (gcc version 4.7 (GCC) ) #1 SMP PREEMPT Wed Dec 31 14:42:53 PST 2014';
 
 function setPlatform(platform: Platform | string) {
    Object.defineProperty(process, 'platform', { value: platform });
@@ -11,11 +16,12 @@ function setPlatform(platform: Platform | string) {
 test('Reports true if platform is Linux but process version has "Windows"', () => {
    const p: string = process.platform;
    setPlatform(Platform.Linux);
+   fs.__setFileContent(fileProc, wslProcVersion);
    expect(checkIsWSL()).toBe(true);
    setPlatform(p);
 });
 
 test('Reports false otherwise', () => {
-   jest.unmock('fs');
+   fs.__setFileContent(fileProc, '');
    expect(checkIsWSL()).toBe(false);
 });
